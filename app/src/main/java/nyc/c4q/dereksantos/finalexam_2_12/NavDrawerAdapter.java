af0b50dc34cc76219d2dfc33d2e1d2fd8735fef1
sderekj <dereksantos1989@gmail.com>
2017-02-12 18:00:55 -0500

    Modified Activity Profile xml

--- app/src/main/java/nyc/c4q/dereksantos/finalexam_2_12/NavDrawerAdapter.java
+++ app/src/main/java/nyc/c4q/dereksantos/finalexam_2_12/NavDrawerAdapter.java
@@ -1,17 +1,10 @@
 package nyc.c4q.dereksantos.finalexam_2_12;
 
 import android.support.v7.widget.RecyclerView;
-import android.view.View;
 import android.view.ViewGroup;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
-
-import static nyc.c4q.dereksantos.finalexam_2_12.R.id.button1;
-import static nyc.c4q.dereksantos.finalexam_2_12.R.id.button2;
-import static nyc.c4q.dereksantos.finalexam_2_12.R.id.button3;
-import static nyc.c4q.dereksantos.finalexam_2_12.R.id.button4;
 
 /**
  * Created by SACC on 2/12/17.
--- app/src/main/java/nyc/c4q/dereksantos/finalexam_2_12/ProfileActivity.java
+++ app/src/main/java/nyc/c4q/dereksantos/finalexam_2_12/ProfileActivity.java
@@ -19,10 +19,10 @@ public class ProfileActivity extends AppCompatActivity implements View.OnClickLi
 
     private List<NavDrawerItem> itemList = new ArrayList<>();
     private RecyclerView recyclerView;
-    private ImageButton button1;
-    private ImageButton button2;
-    private ImageButton button3;
-    private ImageButton button4;
+    private ImageButton heart_button;
+    private ImageButton message_button;
+    private ImageButton settings_button;
+    private ImageButton logout_button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,44 +31,60 @@ public class ProfileActivity extends AppCompatActivity implements View.OnClickLi
 
         setTitle("Profile");
 
-        recyclerView = (RecyclerView) findViewById(R.id.nav_drawer_rv);
-        recyclerView.setLayoutManager(new LinearLayoutManager(getApplicationContext()));
-        recyclerView.setAdapter(new NavDrawerAdapter());
+        initRecyclerView();
 
-        button1 = (ImageButton) findViewById(R.id.button1);
-        button2 = (ImageButton) findViewById(R.id.button2);
-        button3 = (ImageButton) findViewById(R.id.button3);
-        button4 = (ImageButton) findViewById(R.id.button4);
+        findButtons();
 
-        button1.setOnClickListener(this);
-        button2.setOnClickListener(this);
-        button3.setOnClickListener(this);
-        button4.setOnClickListener(this);
+        setButtonOnClicks();
 
-        itemList.add(new NavDrawerItem(button1.getId(), R.drawable.ic_favorite_black_24dp, "Button 1"));
-        itemList.add(new NavDrawerItem(button2.getId(), R.drawable.ic_email_black_24dp, "Button 2"));
-        itemList.add(new NavDrawerItem(button3.getId(), R.drawable.ic_settings_black_24dp, "Button 3"));
-        itemList.add(new NavDrawerItem(button4.getId(), R.drawable.ic_close_black_24dp, "Logout"));
+        addItemsToList();
 
         NavDrawerAdapter adapter = (NavDrawerAdapter) recyclerView.getAdapter();
         adapter.setData(itemList);
 
     }
 
+    private void addItemsToList() {
+        itemList.add(new NavDrawerItem(heart_button.getId(), R.drawable.ic_favorite_black_24dp, "Button 1"));
+        itemList.add(new NavDrawerItem(message_button.getId(), R.drawable.ic_email_black_24dp, "Button 2"));
+        itemList.add(new NavDrawerItem(settings_button.getId(), R.drawable.ic_settings_black_24dp, "Button 3"));
+        itemList.add(new NavDrawerItem(logout_button.getId(), R.drawable.ic_close_black_24dp, "Logout"));
+    }
+
+    private void setButtonOnClicks() {
+        heart_button.setOnClickListener(this);
+        message_button.setOnClickListener(this);
+        settings_button.setOnClickListener(this);
+        logout_button.setOnClickListener(this);
+    }
+
+    private void findButtons() {
+        heart_button = (ImageButton) findViewById(R.id.heart_button);
+        message_button = (ImageButton) findViewById(R.id.message_button);
+        settings_button = (ImageButton) findViewById(R.id.settings_button);
+        logout_button = (ImageButton) findViewById(R.id.logout_button);
+    }
+
+    private void initRecyclerView() {
+        recyclerView = (RecyclerView) findViewById(R.id.nav_drawer_rv);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getApplicationContext()));
+        recyclerView.setAdapter(new NavDrawerAdapter());
+    }
+
     @Override
     public void onClick(View view) {
         switch (view.getId()) {
-            case R.id.button1:
+            case R.id.heart_button:
                 Toast.makeText(this, "Selected Heart Activity", Toast.LENGTH_SHORT).show();
                 break;
-            case R.id.button2:
+            case R.id.message_button:
                 Toast.makeText(this, "Selected Message Activity", Toast.LENGTH_SHORT).show();
                 break;
-            case R.id.button3:
+            case R.id.settings_button:
                 Intent intent = new Intent(getApplicationContext(), SettingsActivity.class);
                 startActivity(intent);
                 break;
-            case R.id.button4:
+            case R.id.logout_button:
                 Toast.makeText(this, "Selected Logout Activity", Toast.LENGTH_SHORT).show();
                 finishAffinity();
                 break;
